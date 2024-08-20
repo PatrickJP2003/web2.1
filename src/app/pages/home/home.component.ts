619c576a74079dbf462b2cc4fed38e570084b2a6
Esteban S. <patricksanchez17estudio@gmai.com>
2024-08-20 10:33:15 -0500

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Pokemon } from '../utils/pokemon';
+import * as pokemonData from '../../../../public/json/pokemnData.json'
+import { CommonModule } from '@angular/common';
+
+function randomInt(max: number){
+  return Math.floor(Math.random()*max);
+}
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  pokemons: Pokemon[]= (pokemonData  as any).default;
+  indicePokemonSemana: number= randomInt(3);
+
+  constructor(){}
+
+  ngOnInit():void{
+    console.log(pokemonData);
+  }
+
+}
